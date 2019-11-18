06a5bdd49e14771421a9b84583233bf758fe5a4f
pengding1994 <ding96@purdue.edu>
2019-11-17 19:45:56 -0500

    build

---
+++ build/precache-manifest.56d42a6b9773f6d7eeea49fcf465783e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5174f9c6028364699c430eee863eab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86d62e898eb8e23ff193",
+    "url": "/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "7002b8f88eb3407935d4",
+    "url": "/static/css/main.f447de4b.chunk.css"
+  },
+  {
+    "revision": "86d62e898eb8e23ff193",
+    "url": "/static/js/2.c1fe4422.chunk.js"
+  },
+  {
+    "revision": "7002b8f88eb3407935d4",
+    "url": "/static/js/main.2b687cb6.chunk.js"
+  },
+  {
+    "revision": "8569d66e2df6dfc359e2",
+    "url": "/static/js/runtime-main.40fe5e22.js"
+  },
+  {
+    "revision": "9470e2d5c6c7f425edc8e9d8baebdb05",
+    "url": "/static/media/nba-logoman-word-white.9470e2d5.svg"
+  }
+]);
